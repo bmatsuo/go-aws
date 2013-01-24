99b705c400c3fa6447d90424178d6c0d3a966832
Bryan Matsuo <bryan.matsuo@gmail.com>
2013-01-23 23:36:58 -0800

    Empty project generated by gonew.

---
+++ aws.go
@@ -0,0 +1,16 @@
+// Copyright 2013, Bryan Matsuo. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+/*  Filename:    aws.go
+ *  Author:      Bryan Matsuo <bryan.matsuo [at] gmail.com>
+ *  Created:     2013-01-23 23:36:58.888125 -0800 PST
+ *  Description: Main source file in go-aws
+ */
+
+
+// Package aws does ....
+package aws
+
+import (
+)
---
+++ aws_test.go
@@ -0,0 +1,21 @@
+// Copyright 2013, Bryan Matsuo. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package aws
+
+/*  Filename:    aws_test.go
+ *  Author:      Bryan Matsuo <bryan.matsuo [at] gmail.com>
+ *  Created:     2013-01-23 23:36:58.888344 -0800 PST
+ *  Description: Main test file for go-aws
+ */
+
+import (
+    "testing"
+)
+
+
+func TestAws(t *testing.T) {
+
+}
+
